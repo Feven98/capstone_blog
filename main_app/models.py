2b78c7b21896a61f65cd7e15bc167ac9fed1ee10
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-30 23:43:36 -0500

    create first blog

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=150)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.title 
+        # return self.title + ' | ' + str(self.writer)
+
+    class Meta:
+        ordering = ['title']
--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from .models import Blog
 # Create your views here.
 
-class Home(TemplateView):
+# class Home(TemplateView):
+#     template_name = "home.html"
+
+class Home(ListView):
+    model = Blog
     template_name = "home.html"
+
 
 class About(TemplateView):
     template_name = "about.html"
+
