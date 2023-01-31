ddb7b55c8111b075bcb9089a5f43637faf7d116b
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-31 13:35:43 -0500

    create first post from website

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
     path('blog/<int:pk>', views.BlogDetail.as_view(), name="blog_detail"),
+    path('blog/new', views.BlogCreate.as_view(), name= "blog_create"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse
 from .models import Blog
 # Create your views here.
 
@@ -20,3 +22,11 @@ class About(TemplateView):  # not sure what to include but needed
 class BlogDetail(DetailView):  # Blog detail page
     model = Blog
     template_name = "blog_detail.html"
+
+class BlogCreate(CreateView):
+    model = Blog
+    fields = ['title', 'writer', 'content']
+    template_name = "blog_create.html"
+
+    def get_success_url(self):
+                return reverse('blog_detail', kwargs={'pk':self.object.pk})
