b54b52e0a56a13b261d84fdb6c95afbc56be031a
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-02-01 16:27:36 -0500

    add some data to model

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,7 @@ class Blog(models.Model):
     # writer = models.TextField(max_length=300)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=300)
-    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1, related_name="title")
 
     def __str__(self):
