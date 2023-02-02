a6879bcf83ed12a9744f80ed4ceb2e54f8009d14
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-02-02 15:27:35 -0500

    add some env file

--- guzo_blog/settings.py
+++ guzo_blog/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.shortcuts import render, redirect
 from decouple import config
 import os
+import environ
 
+env= environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,6 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+AWS_ACCESS_KEY_ID= env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY= env('AWS_SECRET_ACCESS_KEY')
+S3_BUCKET= env('S3_BUCKET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
